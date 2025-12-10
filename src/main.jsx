058b049e301f74518eb3c0d8be4840c4227d8f34
VJ2569 <vjuriyani25@gmail.com>
2025-12-10 13:45:59 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PropertyDatabase from './PropertyDatabase'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PropertyDatabase />
+  </React.StrictMode>,
+)
